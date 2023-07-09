5c79422894eab5b97665cb223b4d01468f52827e
laavanya11 <laavanya113@gmail.com>
2023-07-10 02:06:07 +0530

    Js Done

---
+++ App.js
@@ -0,0 +1,17 @@
+let celsius=document.getElementById("celsius");
+let fahrenheit=document.getElementById("fahrenheit");
+
+function CelToFar()
+{
+    let result =(parseFloat(celsius.value)*9/5)+32;
+    fahrenheit.value=parseFloat(result.toFixed(2));
+    console.log(result); //for terminal 
+}
+
+function FarToCel()
+{
+    let result =(parseFloat(fahrenheit.value)-32)*5/9;
+    celsius.value=parseFloat(result.toFixed(2));
+    console.log(result); //for terminal
+}
+
